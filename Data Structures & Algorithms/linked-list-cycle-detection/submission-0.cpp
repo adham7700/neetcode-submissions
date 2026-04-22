e98dc21c0de8d8fb7178d42facf09e85ad61b6d1
Adham Mohamed <108670416+adham7700@users.noreply.github.com>
2026-04-22 09:17:04 +0200

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        map<ListNode*,int>note;
+        ListNode *curr=head;
+        while(curr!=nullptr){
+            auto it=note.find(curr);
+
+            if(it!=note.end()){
+                return true;
+            }
+
+            note[curr]=curr->val;
+            curr=curr->next;
+            
+
+        }
+        return false;
+    }
+};
